685b0c245e7f84f5208085eae6999546fbe20cf7
Arjunsing1997 <acchur1997@gmail.com>
2021-04-25 17:09:58 +0530

    UC1 Adding Person

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -8,9 +8,55 @@ namespace AddressBook
 {
     class Program
     {
+        public static List<Person> People = new List<Person>();
+        public class Person
+        {
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public string Address { get; set; }
+            public string City { get; set; }
+            public string State { get; set; }
+            public string Zip { get; set; }
+            public string PhoneNumber { get; set; }
+            public string Email { get; set; }
+
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ADDRESS BOOK");
+            AddPerson();
+        }
+        private static void AddPerson()
+        {
+            Person person = new Person();
+
+            Console.Write("Enter First Name: ");
+            person.FirstName = Console.ReadLine();
+
+            Console.Write("Enter Last Name: ");
+            person.LastName = Console.ReadLine();
+
+            Console.Write("Enter Address : ");
+            person.Address = Console.ReadLine();
+
+            Console.Write("Enter City: ");
+            person.City = Console.ReadLine();
+
+            Console.Write("Enter State : ");
+            person.State = Console.ReadLine();
+
+            Console.Write("Enter ZIP : ");
+            person.Zip = Console.ReadLine();
+
+            Console.Write("Enter Phone Number: ");
+            person.PhoneNumber = Console.ReadLine();
+
+            Console.Write("Enter Email : ");
+            person.Email = Console.ReadLine();
+
+
+
+            People.Add(person);
         }
     }
 }
