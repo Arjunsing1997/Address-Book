b3209a7acec18444d0b392913fcb7055c326da90
Arjunsing1997 <acchur1997@gmail.com>
2021-04-25 17:35:20 +0530

    UC2- adding more Details to the Book

--- AddressBook/AddressBook/Program.cs
+++ AddressBook/AddressBook/Program.cs
@@ -21,42 +21,87 @@ namespace AddressBook
             public string Email { get; set; }
 
         }
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to ADDRESS BOOK");
-            AddPerson();
+            string command = "";
+            while (command != "exit")
+            {
+                Console.Clear();
+                Console.WriteLine("Please enter a command: ");
+                Console.WriteLine("ADD");
+                Console.WriteLine("LIST");
+                command = Console.ReadLine().ToLower();
+                switch (command)
+                {
+                    case "add":
+                        AddPerson();
+                        break;
+                    case "list":
+                        ListPeople();
+                        break;
+                }
+            }
         }
-        private static void AddPerson()
+            private static void AddPerson()
+            {
+                Person person = new Person();
+
+                Console.Write("Enter First Name: ");
+                person.FirstName = Console.ReadLine();
+
+                Console.Write("Enter Last Name: ");
+                person.LastName = Console.ReadLine();
+
+                Console.Write("Enter Address : ");
+                person.Address = Console.ReadLine();
+
+                Console.Write("Enter City: ");
+                person.City = Console.ReadLine();
+
+                Console.Write("Enter State : ");
+                person.State = Console.ReadLine();
+
+                Console.Write("Enter ZIP : ");
+                person.Zip = Console.ReadLine();
+
+                Console.Write("Enter Phone Number: ");
+                person.PhoneNumber = Console.ReadLine();
+
+                Console.Write("Enter Email : ");
+                person.Email = Console.ReadLine();
+
+                People.Add(person);
+            }
+         private static void ListPeople()
         {
-            Person person = new Person();
+            if (People.Count == 0)
+            {
+                Console.WriteLine("Your address book is empty. Press any key to continue.");
+                Console.ReadKey();
+                return;
+            }
+            Console.WriteLine("Here are the current people in your address book:\n");
+            foreach (var person in People)
+            {
+                PrintPerson(person);
+            }
+            Console.WriteLine("\nPress any key to continue.");
+            Console.ReadKey();
+        }
 
-            Console.Write("Enter First Name: ");
-            person.FirstName = Console.ReadLine();
+        private static void PrintPerson(Person person)
+        {
+            Console.WriteLine("First Name: " + person.FirstName);
+            Console.WriteLine("Last Name: " + person.LastName);
+            Console.WriteLine("Address : " + person.Address);
+            Console.WriteLine("City: "+person.City);
+            Console.WriteLine("Satet: "+person.State);
+            Console.WriteLine("ZIP: "+person.Zip);
+            Console.WriteLine("Phone Number: " + person.PhoneNumber);
+            Console.WriteLine("Email :"+person.Email);
 
-            Console.Write("Enter Last Name: ");
-            person.LastName = Console.ReadLine();
-
-            Console.Write("Enter Address : ");
-            person.Address = Console.ReadLine();
-
-            Console.Write("Enter City: ");
-            person.City = Console.ReadLine();
-
-            Console.Write("Enter State : ");
-            person.State = Console.ReadLine();
-
-            Console.Write("Enter ZIP : ");
-            person.Zip = Console.ReadLine();
-
-            Console.Write("Enter Phone Number: ");
-            person.PhoneNumber = Console.ReadLine();
-
-            Console.Write("Enter Email : ");
-            person.Email = Console.ReadLine();
-
-
-
-            People.Add(person);
+            Console.WriteLine("-------------------------------------------");
         }
     }
 }
